625332563d269879f3ba508d1e46074d814a4b1a
Илья Гусев <78803025+badasqi@users.noreply.github.com>
2023-03-01 23:48:15 +0400

    Ilya-CD-010

--- cd_main/views.py
+++ cd_main/views.py
@@ -8,7 +8,7 @@ class ProjectAPIView(APIView):
         return Response({'project_id': '1', 'title': 'Турболыжи', 'description': 'МЫ В РЕДАНЕ', 'type_id': '1', 'created_at': '01.03.2023', 'skills_ids': [1,2,3]})
 class ProjectTypeAPIView(APIView):
     def get(self, request):
-        return Response({'Рекрутинговый портал'})
+        return Response({'value' :' Рекрутинговый портал'})
 class ProjectSkillAPIView(APIView):
     def get(self, request):
-        return Response({'Django REST Framework'})
+        return Response({'valuse' :'Django REST Framework'})
