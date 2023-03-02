652a43b72f8e671aaedaafec81322530cad150f9
Илья Гусев <78803025+badasqi@users.noreply.github.com>
2023-03-02 18:14:31 +0400

    Ilya-CD-010

--- cd_main/views.py
+++ cd_main/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import *
+import datetime
+from django.forms import model_to_dict
 # Create your views here.
 
 class ProjectAPIView(APIView):
     def get(self, request):
-        return Response({'project_id': '1', 'title': 'Турболыжи', 'description': 'МЫ В РЕДАНЕ', 'type_id': '1', 'created_at': '01.03.2023', 'skills_ids': [1,2,3]})
+        lst = Project.objects.all().values()
+        return Response({'Projects':list(lst)})
+    
+    def post(self, request):
+        project_new = Project.objects.create(
+            title = request.data['title'],
+            description = request.data['description'],
+            created_at = request.data['created_at'],
+            project_type_id = request.data['project_type_id']
+        )
+        return Response({'Project': model_to_dict(project_new)})
 class ProjectTypeAPIView(APIView):
     def get(self, request):
         return Response({'value' :' Рекрутинговый портал'})
