ef43fdc3a039cd2a3077c5ae3eaa8de564340c91
Илья Гусев <78803025+badasqi@users.noreply.github.com>
2023-03-02 21:53:59 +0400

    Ilya-CD-010

--- cd_main/serializers.py
+++ cd_main/serializers.py
@@ -2,31 +2,17 @@ from rest_framework import serializers
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from .models import *
+from djongo import models
+from django import forms
 import io
-class ProjectModel:
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
 
 class ProjectSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=100)
     description = serializers.CharField(max_length=5000)
-    #created_at = models.DateTimeField(auto_now_add=True)
-    #update_at = models.DateTimeField(auto_now=True)
-    #soft_delete = models.BooleanField(default=False)
-
-class Meta:
-    model = Project
-    fields = ('title', 'description')
-
-def encode():
-    model = ProjectModel('title: BuySell1', 'description: Redan MOSH')
-    #'created_at':'02.03.20231',
-    #'project_type_id':41
-    model_sr = ProjectSerializer(model)
-    json = JSONRenderer().render(model_sr.data)
-    print(json)
-
-def decode():
-    stream = io.BytesIO(b'{"title":"title: BuySell1","description":"description: \xd0\xa0\xd0\x95\xd0\x94\xd0\x90\xd0\x9d \xd0\xa1\xd0\x98\xd0\x9b\xd0\x901"}')
-    data = JSONParser().parse(stream)
+    skills = serializers.ListField()
+    created_at = serializers.DateTimeField(read_only=True)
+    update_at = serializers.DateTimeField(read_only=True)
+    soft_delete = serializers.BooleanField()
+    
+    def create(self, validated_data):
+        return Project.objects.create(**validated_data)
--- cd_main/views.py
+++ cd_main/views.py
@@ -4,21 +4,20 @@ from rest_framework.response import Response
 from .models import *
 import datetime
 from django.forms import model_to_dict
+from .serializers import *
 # Create your views here.
 
 class ProjectAPIView(APIView):
     def get(self, request):
-        lst = Project.objects.all().values()
-        return Response({'Projects':list(lst)})
+        projects = Project.objects.all()
+        return Response({'Projects':ProjectSerializer(projects, many=True).data})
     
     def post(self, request):
-        project_new = Project.objects.create(
-            title = request.data['title'],
-            description = request.data['description'],
-            created_at = request.data['created_at'],
-            project_type_id = request.data['project_type_id']
-        )
-        return Response({'Project': model_to_dict(project_new)})
+        serializer = ProjectSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'Project': serializer.data})
+    
 class ProjectTypeAPIView(APIView):
     def get(self, request):
         return Response({'value' :' Рекрутинговый портал'})
