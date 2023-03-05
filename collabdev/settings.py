c8f02d6552d33ddd1b09ad07e3c97899ea6d1705
Илья Гусев <78803025+badasqi@users.noreply.github.com>
2023-03-05 20:25:30 +0400

    Ilya-CD-010

--- collabdev/settings.py
+++ collabdev/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cd_main'
+    'cd_main',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
