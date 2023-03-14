2363185ad33752585f83fc796f0a12862ecb8847
Илья Гусев <78803025+badasqi@users.noreply.github.com>
2023-03-15 01:13:23 +0400

    Ilya-CD-024

--- cd_main/views.py
+++ cd_main/views.py
@@ -1,29 +1,21 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import *
 import datetime
 from django.forms import model_to_dict
 from .serializers import *
 # Create your views here.
 
-class ProjectAPIView(APIView):
-    def get(self, request):
-        projects = Project.objects.all().filter(soft_delete__in=[False])
-        return Response({'Projects':ProjectSerializer(projects, many=True).data})
-
-    def post(self, request):
-        serializer = ProjectSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+class ProjectAPIView(generics.ListAPIView, generics.ListCreateAPIView):
+    queryset = Project.objects.all().filter(soft_delete__in=[False]) 
+    serializer_class = ProjectSerializer
+    def perform_create(self,serializer):
         serializer.save()
-        return Response({'Project': serializer.data})
 
-class ProjectTypeAPIView(APIView):
-    def get(self, request):
-        types = ProjectTypes.objects.all()
-        return Response({'ProjectTypes' :ProjectTypesSerializer(types, many=True).data})
+class ProjectTypeAPIView(generics.ListAPIView):
+    queryset = ProjectTypes.objects.all()
+    serializer_class = ProjectTypesSerializer
 
-class ProjectSkillAPIView(APIView):
-    def get(self, request):
-        skills = Skill.objects.all()
-        return Response({'ProjectSkills' : SkillsSerializer(skills, many=True).data})
+class ProjectSkillAPIView(generics.ListAPIView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillsSerializer
