0f00694f7e7eeddf8b38b2817bf319f157fcc065
Andrei Arnautov <aleksejtalitej@gmail.com>
2023-03-17 22:03:14 +0400

    JWT settings

--- cd_main/serializers.py
+++ cd_main/serializers.py
@@ -4,22 +4,26 @@ from rest_framework.parsers import JSONParser
 from .models import *
 
 
-
 class ProjectSerializer(serializers.ModelSerializer):
     skills = serializers.PrimaryKeyRelatedField(queryset=Skill.objects.all(), many=True)
     project_type = serializers.PrimaryKeyRelatedField(queryset=ProjectTypes.objects.all())
+
     class Meta:
         model = Project
         fields = ("title", "description", "skills", "created_at", "updated_at", "soft_delete", "project_type")
 
+
 class SkillsSerializer(serializers.ModelSerializer):
-    skills = ProjectSerializer(many=True,read_only=True)
+    skills = ProjectSerializer(many=True, read_only=True)
+
     class Meta:
         model = Skill
         fields = ("skills", "title")
 
+
 class ProjectTypesSerializer(serializers.ModelSerializer):
     types = ProjectSerializer(read_only=True)
+
     class Meta:
         model = ProjectTypes
         fields = '__all__'
--- cd_main/views.py
+++ cd_main/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
 from rest_framework import generics
-from .models import *
-import datetime
-from django.forms import model_to_dict
+
 from .serializers import *
 
 
